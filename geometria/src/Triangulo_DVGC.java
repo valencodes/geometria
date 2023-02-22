8b105aeb7a218b5ae68166b63daa464daf09edaf
Valentina <valentinagaleote@gmail.com>
2023-02-22 22:57:20 +0100

    método en una línea

--- geometria/src/Triangulo_DVGC.java
+++ geometria/src/Triangulo_DVGC.java
@@ -19,11 +19,7 @@ public class Triangulo_DVGC extends FiguraGeometrica_DVGC {
 	@Override
 	public double area() { //Utiliza la formula de Her�n
 		double sp;
-		sp = this.semiPerimetro();
+		sp = this.perimetro() / 2;
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
-	}
-	
-	public double semiPerimetro() {
-		return this.perimetro() / 2;
 	}
 }
