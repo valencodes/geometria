5714058c6a902e782588c118260dd3cd147ae9bb
Valentina <valentinagaleote@gmail.com>
2023-02-22 22:56:09 +0100

    versión refactorizada

--- geometria/src/FiguraGeometrica_DVGC.java
+++ geometria/src/FiguraGeometrica_DVGC.java
@@ -1,9 +1,9 @@
 
-public abstract class FiguraGeometrica {
+public abstract class FiguraGeometrica_DVGC {
 
 	protected String tipoFigura;
 
-	public FiguraGeometrica(String tipoFigura) {
+	public FiguraGeometrica_DVGC(String tipoFigura) {
 		super();
 		this.tipoFigura = tipoFigura;
 	}
