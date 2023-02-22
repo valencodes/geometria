8f1181c547496b66d48c41ac7bec7ae764ad093e
Valentina <valentinagaleote@gmail.com>
2023-02-22 23:03:57 +0100

    nuevo método

--- geometria/src/Triangulo_DVGC.java
+++ geometria/src/Triangulo_DVGC.java
@@ -17,9 +17,10 @@ public class Triangulo_DVGC extends FiguraGeometrica_DVGC {
 	}
 
 	@Override
-	public double area() { //Utiliza la formula de Her�n
+	public double area() { //Utiliza la formula de Her—n
 		double sp;
-		sp = this.perimetro() / 2;
+		sp = this.semiPerimetro();
 		return Math.sqrt(sp * (sp-lado1) * (sp-lado2) * (sp-lado3));
 	}
+	
 }
