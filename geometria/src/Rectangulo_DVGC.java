c28b2b6db9e0ab0465ab17797f20dd210e0bbcb1
Valentina <valentinagaleote@gmail.com>
2023-02-22 23:08:02 +0100

    versión comentada

--- geometria/src/Rectangulo_DVGC.java
+++ geometria/src/Rectangulo_DVGC.java
@@ -1,20 +1,36 @@
-
+/**
+ * @author Daniela Valentina Galeote Calderón
+ * @version 1.2
+ */
 public class Rectangulo_DVGC extends FiguraGeometrica_DVGC {
 	private double l1;
 	private double l2;
-	
+	/**
+	 * Constructor con los dos lados del rectángulo
+	 * @param tipoFigura
+	 * @param lG
+	 * @param lP
+	 */
 	public Rectangulo_DVGC(String tipoFigura, double lG, double lP) {
 		super(tipoFigura);
 		l1 = lG;
 		l2 = lP;
 	}
 
+
 	@Override
+	/**
+	 * @return devuelve area del rectángulo
+	 */
 	public double area() {
 		return l1 * l2;
 	}
 	
+	
 	@Override
+	/**
+	 * @return el perímetro del rectángulo
+	 */
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
 	}
